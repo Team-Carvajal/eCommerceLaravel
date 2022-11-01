b7b3c4c9d2ae4345352f9d2cda1fb7d59e8dd965
Mawfyy <mawfyy12@gmail.com>
2022-10-31 20:40:16 -0500

    changes

--- ping_eCommerce/routes/web.php
+++ ping_eCommerce/routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('administracion');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Routes::get('/login', function(){
+  return view('login');
+})
+
+
+
+require __DIR__ . '/auth.php';
