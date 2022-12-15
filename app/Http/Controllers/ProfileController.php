5e51bdbd5af2aa4458acb4d05bb287e548c882ac
mawfyy <mawfyy12@gmail.com>
2022-12-14 20:00:20 -0500

    Reset password added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -29,7 +29,7 @@ class ProfileController extends Controller
     return $status === Password::RESET_LINK_SENT
                 ? back()->with(['status' => __($status)])
                 : back()->withErrors(['email' => __($status)]);
-
+    
     }
 
     public function resetPassword(Request $request){
@@ -60,6 +60,7 @@ class ProfileController extends Controller
     return $status === Password::PASSWORD_RESET
                 ? redirect()->route('home')->with('status', __($status))
                 : back()->withErrors(['email' => [__($status)]]);
+    
     }
 
 
